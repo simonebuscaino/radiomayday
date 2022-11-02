c6afcdd9ce40642d4096903652bb5e97e9892b88
Simone Buscaino <Simone Buscaino>
2022-11-03 00:15:42 +0100

    fix

--- src/pages/StaffScreen/StaffScreen.jsx
+++ src/pages/StaffScreen/StaffScreen.jsx
@@ -1,35 +1,82 @@
 import React, { useEffect, useState } from "react";
 import {Container, Row, Col, Image} from "react-bootstrap";
 import "./StaffScreen.css";
-// import {staff} from "./staff";
+import {staff, voci} from "./staff";
 import {db} from "../../firebase";
 import {onSnapshot, collection, getDocs, query, orderBy} from "firebase/firestore";
 import * as Icon from 'react-bootstrap-icons';
 
 function StaffScreen () {
 
-    const [staff, setStaff] = useState();
-    const staffCollectionRef = collection(db, "staff");
-    const q = query(staffCollectionRef, orderBy("name", "asc"))
-    useEffect(()=>{
-        onSnapshot(q, (snapshot)=> {
-            let crew = [];
-            snapshot.docs.forEach((doc)=>{
-                crew.push({...doc.data(), id: doc.id})
-            })
-            setStaff(crew);
-        })
-    }, []);
+    const [dataStaff, setDataStaff] = useState(staff);
+    // const staffCollectionRef = collection(db, "staff");
+    // const q = query(staffCollectionRef, orderBy("name", "asc"))
+    // useEffect(()=>{
+    //     onSnapshot(q, (snapshot)=> {
+    //         let crew = [];
+    //         snapshot.docs.forEach((doc)=>{
+    //             crew.push({...doc.data(), id: doc.id})
+    //         })
+    //         setStaff(crew);
+    //     })
+    // }, []);
     
 
     return (
         <Container fluid className="containerBody">
             <Row>
                 <Col>
-                    <h1>Staff</h1>
+                    <h1 className="p-2 bg-gradient text-white">Staff</h1>
                 </Col>
             </Row>
             <Row className="mt-4 mb-3">
+                {
+                    dataStaff.map(el => (
+                        <Col md="3" key={el.id}>
+                            <Row className="containerBoxStaff m-2">
+                                <Image className="imgNotPadding mb-2" src={el.img} width="100%" rounded />
+                                <h4><strong>{el.name}</strong></h4>
+                                <h5>{el.roles}</h5>
+                                <span>
+                                    <Icon.Facebook size="24px" />
+                                    &nbsp;&nbsp;
+                                    <Icon.Instagram size="24px" />
+                                </span>
+                            </Row>
+                        </Col>
+                    ))
+                }
+            </Row>
+            <Row className="mt-4 mb-3">
+                <Col md="12">
+                    <h2 className="p-2 bg-gradient text-white">Le nostre voci</h2>
+                </Col>
+                {
+                    voci.map(el => (
+                        <Col md="6" key={el.id}>
+                            <Row className="containerBoxStaff m-2 text-start">
+                                <Col md="4">
+                                    <Image className="imgNotPadding mb-2" src={el.img} width="100%" rounded />
+                                </Col>
+                                <Col md="8">
+                                    <h4><strong>{el.name}</strong></h4>
+                                    <h5>{el.roles}</h5>
+                                    <em>La vostra sveglia del mattino qui per voi!</em>
+                                    <br /><br /> 
+                                    <span>
+                                        <Icon.Facebook size="24px" />
+                                        {" "}
+                                        <Icon.Instagram size="24px" />
+                                    </span>
+                                </Col>
+
+                            </Row>
+                        </Col>
+                    ))
+                }
+            </Row>
+            
+            {/* <Row className="mt-4 mb-3">
                 {
                     staff === undefined || staff.length === 0 ?
                         <h6>Non risultano presenti membri dello Staff</h6>
@@ -50,7 +97,7 @@ function StaffScreen () {
                         </Col>
                     ))
                 }
-            </Row>
+            </Row> */}
         </Container>
     )
 }
--- src/pages/StaffScreen/staff.js
+++ src/pages/StaffScreen/staff.js
@@ -1,32 +1,25 @@
 export const staff = [
     {
-        img: "https://firebasestorage.googleapis.com/v0/b/kosmos-radio.appspot.com/o/staff%2Fchristian_casola.jpg?alt=media&token=0e7b70b5-4626-4e27-b9f0-321ad41bbbf1",
-        name: "Christian Casola",
-        roles: "Conduttore Radiofonico",
+        img: "img/staff/eduardo_castaldo.png",
+        name: "Eduardo Castaldo",
+        roles: "Editore & Station Manager",
     },
     {
-        img: "https://firebasestorage.googleapis.com/v0/b/radio-regione-campania.appspot.com/o/STAFF%20-%20Alicia.jpg?alt=media&token=800c858d-3008-4556-951b-5cb42c011a2f",
-        name: "Alicia",
-        roles: "Conduttrice Radiofonica",
+        img: "img/staff/simone_buscaino.png",
+        name: "Simone Buscaino",
+        roles: "Station Manager & Web Developer",
+    },
+];
+
+export const voci = [
+    {
+        img: "img/staff/antonio_castaldo.png",
+        name: "Antonio Castaldo",
+        roles: "Speaker",
     },
     {
-        img: "https://firebasestorage.googleapis.com/v0/b/radio-regione-campania.appspot.com/o/STAFF%20-%20Armando%20Rufolo.jpg?alt=media&token=4e9480e6-27a2-49f1-9b40-bd1e2f4398dd",
-        name: "Armando Rufolo",
-        roles: "Conduttore Radiofonico",
+        img: "img/staff/antonio_castaldo.png",
+        name: "Antonio Castaldo",
+        roles: "Speaker",
     },
-    {
-        img: "https://i.imgur.com/NKzGUST.jpg",
-        name: "Sara Visciano",
-        roles: "Conduttrice Radiofonica",
-    },
-    {
-        img: "https://i.imgur.com/NKzGUST.jpg",
-        name: "Sara Visciano",
-        roles: "Conduttrice Radiofonica",
-    },
-    {
-        img: "https://i.imgur.com/NKzGUST.jpg",
-        name: "Sara Visciano",
-        roles: "Conduttrice Radiofonica",
-    }
 ];
