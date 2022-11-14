ca6d5565836aee68e7aedd65cbd0e200f782e2c5
Simone Buscaino <Simone Buscaino>
2022-11-14 10:14:58 +0100

    fix

--- src/pages/ChiSiamoScreen/ChiSiamoScreen.jsx
+++ src/pages/ChiSiamoScreen/ChiSiamoScreen.jsx
@@ -18,8 +18,8 @@ function ChiSiamoScreen () {
                 <Col md="7" className="text-start">
                     <h4>Un progetto rivoluzionario e coraggioso.</h4>
                     <p>
-                        <strong>Tropp Fun Radio</strong> è nata nella Città di Sarno, a seguito del concorso LIBERA LA TUA CREATIVITÀ’ tenutosi Giovedì 14 luglio 2020 presso i giardini di Villa Lanzara. 
-                        Nasce come  Radio interscolastica che offre corsi  di radio  gratuiti ai ragazzi  del territorio (conduzione, regia, djing,  ecc.) e raccoglie da  subito il pieno consenso delle Scuole locali  e di tanti amici imprenditori che hanno deciso di sostenere e di far crescere l’iniziativa. In seguito grazie alla fusone con l’emittente <strong>RRC</strong>, acquisisce la possibilità tramite concessione ministeriale di trasmettere anche in DAB+ (Digital Audio Broadcasting) in tutta la regione Campania.<br/>
+                        <strong>Tropp Fun Radio</strong> è nata nella Città di Sarno, a seguito del concorso LIBERA LA TUA CREATIVITÀ’ tenutosi Giovedì 14 luglio 2022 presso i giardini di Villa Lanzara. 
+                        Nasce come  Radio interscolastica che offre corsi  di radio  gratuiti ai ragazzi  del territorio (conduzione, regia, djing,  ecc.) e raccoglie da  subito il pieno consenso delle Scuole locali  e di tanti amici imprenditori che hanno deciso di sostenere e di far crescere l’iniziativa. In seguito grazie alla fusione con l’emittente <strong>RRC</strong>, acquisisce la possibilità tramite concessione ministeriale di trasmettere anche in DAB+ (Digital Audio Broadcasting) in tutta la regione Campania.<br/>
                         Lo staff di Tropp Fun Radio è  composto da ragazzi provenienti dai corsi di formazione (giovanissimi), ma anche da persone con  esperienza in ambito radiofonico che collaborano da tutta Italia, tutti pronti a portare una ventata di aria fresca nell’ambito dell’entertainement.
                     </p>
                 </Col>
